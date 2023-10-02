1d4f7865ec72d4926c53a354b352e1d3757252bd
John Levermore <john.levermore@zhibek.com>
2023-10-02 11:05:17 +0100

    Refactor Downloader stream pipes to pipeline to ensure pipes have completed before returning

--- data/src/Service/Downloader.js
+++ data/src/Service/Downloader.js
@@ -1,13 +1,12 @@
 import { Readable } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
 import fs from 'fs';
 import bz2 from 'unbzip2-stream';
 
-import Wait from '../Utils/Wait.js';
 import {
   WIKIVOYAGE_DUMP_URL,
   WIKIVOYAGE_DUMP_PATH,
 } from '../Config/Constants.js';
-
 
 const Downloader = async (force = false) => {
   if (!force && fs.existsSync(WIKIVOYAGE_DUMP_PATH)) {
@@ -16,18 +15,19 @@ const Downloader = async (force = false) => {
   }
 
   console.log('Downloader: Starting...');
+
   const result = await fetch(WIKIVOYAGE_DUMP_URL);
   const blob = await result.blob();
   const webStream = blob.stream();
+
   const readStream = Readable.fromWeb(webStream);
-
   const writeStream = fs.createWriteStream(WIKIVOYAGE_DUMP_PATH);
-  readStream
-    .pipe(bz2())
-    .pipe(writeStream);
 
-  // Add brief wait to allow file-system to update
-  await Wait(500);
+  await pipeline(
+    readStream,
+    bz2(),
+    writeStream
+  );
 
   console.log('Downloader: Completed!');
 };
--- data/src/Utils/Wait.js
+++
@@ -1,5 +0,0 @@
-const Wait = async (ms) => (
-  new Promise(resolve => setTimeout(resolve, ms))
-);
-
-export default Wait;
